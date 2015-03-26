eedd4865d1687c37a3f46b9f184b155be633e300
Abhishek Bhattacharya <abbirdboy@gmail.com>
2015-03-25 19:01:03 -0700

    Added NavBar and Styling

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  require 'rest_client'
+  
+  API_BASE_URL = "http://www.ziglee.com/api/v1/"
+  
+  def new 
+  end 
+  
+  def create
+    
+  end 
+  
+  def update
+  end 
+  
+  def destroy 
+  end 
+  
+  private 
+  
+end
---
+++ config/initializers/her.rb
@@ -0,0 +1,10 @@
+Her::API.setup url: "http://www.ziglee.com/api/v1/" do |c|
+  # Request
+  c.use Faraday::Request::UrlEncoded
+
+  # Response
+  c.use Her::Middleware::DefaultParseJSON
+
+  # Adapter
+  c.use Faraday::Adapter::NetHttp
+end
