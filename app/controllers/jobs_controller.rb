60d4a9d4f0516af94f16a13acbaecad3dfc7405c
Abhishek Bhattacharya <abbirdboy@gmail.com>
2015-04-07 10:59:49 -0700

    AJAX added

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,5 @@
 class JobsController < ApplicationController
-  
+
   def index
     @search = params[:search]
     if params[:search].blank?
@@ -7,19 +7,25 @@ class JobsController < ApplicationController
       @markers = Array.new 
       for i in 0..@jobs.response[:offers].length-1 do 
         @markers[i] = {lng: @jobs.response[:offers][i][:offer][:longitude],
-                                           lat: @jobs.response[:offers][i][:offer][:latitude]} 
-      end  
+                                           lat: @jobs.response[:offers][i][:offer][:latitude], infowindow: "#{i+1}. #{@jobs.response[:offers][i][:offer][:title]}"} 
+      end 
+      respond_to do |format|
+          format.html
+          format.js
+      end 
     else 
       @jobs = Job.search(@search) #would use ipgeocode in production, default = San Ramon, CA
       @markers = Array.new 
       for i in 0..@jobs.response[:offers].length-1 do 
-        @markers[i] = {lng: @jobs.response[:offers][i][:offer][:longitude],lat: @jobs.response[:offers][i][:offer][:latitude]} 
+        @markers[i] = {lng: @jobs.response[:offers][i][:offer][:longitude],
+                                           lat: @jobs.response[:offers][i][:offer][:latitude], infowindow: "#{i+1}. #{@jobs.response[:offers][i][:offer][:title]}"} 
+      end
+      respond_to do |format|
+          format.html
+          format.js
       end  
     end
   end 
-  
-  
-  
   
   
 end
