556963779395654cec79fb01df931006a3bd200d
Abhishek Bhattacharya <abbirdboy@gmail.com>
2015-04-07 21:41:10 -0700

    More changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,5 +75,6 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
+  config.serve_static_assets = true
   config.active_record.dump_schema_after_migration = false
 end
